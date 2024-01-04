0c56d2bedcb7393ad94af23eb7c48d0cf5c4a864
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2024-01-04 23:13:25 +0700

    latihan: sequelize orm

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express')
 
 const app = express();
 const port = 3000;
+const router = require('./routes/index')
 
+app.use(router)
 
 app.get('/', (req, res) => {
     res.send('Hello World! wkwkwkwk')
 })
+
+// GET, POST, PUT, PATCH, DELETE
+
+
+app.get('/about', (req, res) => {
+    res.send('Ini adalah response dari about')
+})
+
 
 
 app.listen(port, () => {
