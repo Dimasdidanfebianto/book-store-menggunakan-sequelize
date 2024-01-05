c8c3aa0bf2c7b424c33edcac2ea9631f443b7d8c
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2024-01-05 22:56:14 +0700

    update:

---
+++ app/controller/booksController.js
@@ -0,0 +1,21 @@
+const { Book } = require('../../models');
+
+const findAllBooks = async (req, res) => {
+    try {
+        const data = await Book.findAll();
+
+        const result = {
+            status: 'success',
+            data
+        };
+        res.json(result)
+        
+    } catch (error) {
+        res.json(error)
+    }
+}
+
+
+module.exports = {
+    findAllBooks,
+}
